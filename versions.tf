903d3f72b6e85e1fe7db815819485ba02d9bb96d
bayupw <bayupw@gmail.com>
2022-04-17 05:52:20 +1200

    update tf version

--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = ">= 0.13.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=4.4.0"
+      version = ">= 3.63"
     }
   }
 }
